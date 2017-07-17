e015a8830e38b309c1f2755f2945bdb328cb0ae1
ayush <ayush.tiwari@knoldus.in>
2017-07-17 18:14:27 +0530

    Submission of the Assingment

--- src/main/scala/com/knoldus/kip/models/Student.scala
+++ src/main/scala/com/knoldus/kip/models/Student.scala
@@ -1,18 +1,74 @@
 package com.knoldus.kip.models
 
-case class Student(id: Int) extends ModelIdentifier{
+case class Student(id: Int, firstName: String, middleName: Option[String], lastName: String, rollNumber: Int, age: Option[Int],
+                   gender: String, enrollemntNumber: Int, address: Option[String] ) extends ModelIdentifier
+{
 
+    def getAddress:String = address.fold("n/a"){_.toString}
+
+    def getMiddleName:String =middleName.map(_.split(" ")(0)).getOrElse("")
 
 }
 
-case class Subject
-case class Course
+case class Subject(id: Int, name: String, maxMarks: Int, obtainedMarks: Float) extends ModelIdentifier
+
+case class Course(id: Int, name: String, category: String, subjects: List[Subject] ) extends ModelIdentifier
 
 
-case class Scoreboard
+case class ScoreCard(id: Int, student: Student, subjects: List[Subject], total: Float, percentage: Float, grade: String ) extends ModelIdentifier
+{
 
-object Scoreboard {
-  def apply(student: Student, subject: Subject): Scoreboard = ???
+  def getSubjectWithHighestMarks : List[Subject] = {
+    val highest = subjects.map(_.obtainedMarks).max
+    subjects.filter(_.obtainedMarks == highest)
+  }
+
+  def getSubjectWithLowestMAarks : List[Subject] = {
+    val lowest = subjects.map(_.obtainedMarks).min
+    subjects.filter(_.obtainedMarks == lowest)
+  }
+
 }
 
-case class CoursePerformance(id: Int) extends ModelIdentifier
+object ScoreCard {
+
+  def apply(student: Student, subjects: List[Subject]): ScoreCard = {
+
+    val total = subjects.map(_.obtainedMarks) sum
+    val percentage = total/subjects.map(_.maxMarks).sum
+
+    val grade = if(percentage >= 95)
+      {
+        "A1"
+      }
+    else if(percentage >= 90)
+      {
+        "A2"
+      }
+    else if(percentage >= 80)
+      {
+        "B1"
+      }
+    else if(percentage >= 70)
+      {
+        "B2"
+      }
+    else if(percentage >= 60)
+      {
+        "C1"
+      }
+    else if(percentage >= 40)
+      {
+        "C2"
+      }
+    else
+      {
+        "Fail"
+      }
+
+    new ScoreCard(student.id, student, subjects, total, percentage, grade)
+  }
+
+}
+
+case class CoursePerformance(id: Int, year: Int, course: Course, scoreCards: List[ScoreCard]) extends ModelIdentifier
--- src/main/scala/com/knoldus/kip/service/Postman.scala
+++ src/main/scala/com/knoldus/kip/service/Postman.scala
@@ -1,10 +1,22 @@
 package com.knoldus.kip.service
 
-import com.knoldus.kip.objective1.models.{Scorecard, Student}
+import com.knoldus.kip.RamDatabase
+import com.knoldus.kip.models.{ScoreCard, CoursePerformance}
+//import com.knoldus.kip.objective1.models.{Scorecard, Student}
 
 trait Postman {
 
 
-  def getTheFirstAddressOfFirstYearPerformance(id: Int) = ???
+  def getTheFirstAddressOfFirstYearPerformance(id: Int) : String =  {
+
+    val coursePerformance: Option[CoursePerformance] = RamDatabase.getById(id)
+
+    val scoreCard : Option[ScoreCard] = coursePerformance.flatMap(_.scoreCards.headOption)
+
+    val address : String = scoreCard.flatMap(_.student.address).getOrElse("Invalid I.D.")
+
+    address
+
+  }
 
 }
